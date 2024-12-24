5dd3f743365e6acd3e7ceebccb2a0e4234401ccf
Destiny823 <johnsondestiny921084@gmail.com>
2024-12-25 00:31:13 +0100

    Update gmail_automation.py
    I need the email address

--- gmail_automation.py
+++ gmail_automation.py
@@ -8,18 +8,18 @@ from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-import random
+import random block assets 
 import time
 from unidecode import unidecode
-
+email block email 
 # Chrome options
 chrome_options = ChromeOptions()
 chrome_options.add_argument("--disable-infobars")
-
+my site allows email in my site
 # WebDriver service
 service = ChromeService('chromedriver.exe')
 driver = webdriver.Chrome(options=chrome_options)
-
+johnsondestiny921084@gmail.com
 french_first_names = [
     "Amélie", "Antoine", "Aurélie", "Benoît", "Camille", "Charles", "Chloé", "Claire", "Clément", "Dominique",
     "Élodie", "Émilie", "Étienne", "Fabien", "François", "Gabriel", "Hélène", "Henri", "Isabelle", "Jules",
@@ -32,7 +32,7 @@ french_first_names = [
     "Jacques", "Jean", "Jeanne", "Joséphine", "Julien", "Laure", "Lucie", "Maëlle", "Marcel", "Martine",
     "Maxime", "Michel", "Nina", "Océane", "Paul", "Perrine", "Quentin", "Romain", "Solène", "Thérèse"
 ]
-
+corrupt 
 french_last_names = [
     "Leroy", "Moreau", "Bernard", "Dubois", "Durand", "Lefebvre", "Mercier", "Dupont", "Fournier", "Lambert",
     "Fontaine", "Rousseau", "Vincent", "Muller", "Lefèvre", "Faure", "André", "Gauthier", "Garcia", "Perrin",
@@ -45,7 +45,7 @@ french_last_names = [
     "Besson", "Lemoine", "Delorme", "Carpentier", "Dumas", "Marin", "Gosselin", "Mallet", "Blondel", "Adam",
     "Durant", "Laporte", "Boutin", "Lacombe", "Navarro", "Langlois", "Deschamps", "Schneider", "Pasquier", "Renaud"
 ]
-
+Samsung galaxy a 8
 # Randomly select a first name and a last name
 your_first_name = random.choice(french_first_names)
 your_last_name = random.choice(french_last_names)
@@ -103,46 +103,4 @@ def fill_form(driver):
         
        # Create custom email
         time.sleep(2)
-        if driver.find_elements(By.ID, "selectionc4") :
-            create_own_option = wait.until(EC.element_to_be_clickable((By.ID,"selectionc4") ))
-            create_own_option.click()
-        
-        create_own_email = wait.until(EC.element_to_be_clickable((By.NAME, "Username")))
-        username_field = driver.find_element(By.NAME, "Username")
-        username_field.clear()
-        username_field.send_keys(your_username)
-        next_button = driver.find_element(By.CLASS_NAME, "VfPpkd-LgbsSe")
-        next_button.click()
-        
-        # Enter and confirm password
-        password_field = wait.until(EC.visibility_of_element_located((By.NAME, "Passwd")))
-        password_field.clear()
-        password_field.send_keys(your_password)
-        # Locate the parent div element with the ID "confirm-passwd"
-        confirm_passwd_div = driver.find_element(By.ID, "confirm-passwd")
-         #Find the input field inside the parent div
-        password_confirmation_field = confirm_passwd_div.find_element(By.NAME, "PasswdAgain")
-        password_confirmation_field.clear()
-        password_confirmation_field.send_keys(your_password)
-        next_button = driver.find_element(By.CLASS_NAME, "VfPpkd-LgbsSe")
-        next_button.click()
-
-        # Skip phone number and recovery email steps
-        skip_buttons = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "button span.VfPpkd-vQzf8d")))
-        for button in skip_buttons:
-            button.click()
-
-        # Agree to terms
-        agree_button = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button span.VfPpkd-vQzf8d")))
-        agree_button.click()
-
-        print(f"Your Gmail successfully created:\n{{\ngmail: {your_username}@gmail.com\npassword: {your_password}\n}}")
-
-    except Exception as e:
-        print("Failed to create your Gmail, Sorry")
-        print(e)
-    finally:
-        driver.quit()
-
-# Execute the function to fill out the form
-fill_form(driver)
+        if driver.find_elements(By.ID, "selection
